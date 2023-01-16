a03b200325d85b6decca6081712b024743232483
unbanksytv <livethelife.tv@gmail.com>
2023-01-17 00:12:25 +0100

    update

--- contracts/mocks/ERC721AssetBackedMock.sol
+++ contracts/mocks/ERC721AssetBackedMock.sol
@@ -1,16 +1,14 @@
-
-
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.4;
 
-import { Floorprice } from "../floorprice.sol";
+import { ERC721AssetBacked } from "../ERC721AssetBacked.sol";
 
 
-contract ERC721AssetBackedMock is Floorprice {
+contract ERC721AssetBackedMock is ERC721AssetBacked {
 
     constructor(address asset_, uint256 backingAmount_, string memory name_, string memory symbol_) 
-    Floorprice(asset_, backingAmount_, name_, symbol_) {}
+    ERC721AssetBacked(asset_, backingAmount_, name_, symbol_) {}
 
     function mint(address to, uint256 tokenId) public {
         _mint(to, tokenId);
