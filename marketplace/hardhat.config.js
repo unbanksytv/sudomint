e7c9858961419ceafef1448c49f832e25cb56b26
unbanksytv <livethelife.tv@gmail.com>
2023-01-18 10:51:42 +0100

    update

---
+++ marketplace/hardhat.config.js
@@ -0,0 +1,13 @@
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+    solidity: {
+      version: '0.8.9',
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 200,
+        },
+      },
+    },
+  };
+  
