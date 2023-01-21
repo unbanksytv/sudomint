00a231e0216018545520bc23f842eb1989fdb4fc
unbanksytv <livethelife.tv@gmail.com>
2023-01-21 23:38:07 +0100

    update

---
+++ contracts/Nouns.sol
@@ -0,0 +1,399 @@
+// SPDX-License-Identifier: GPL-3.0
+
+/// @title The Nouns DAO auction house
+
+/*********************************
+ * ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ *
+ * ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ *
+ * ░░░░░░█████████░░█████████░░░ *
+ * ░░░░░░██░░░████░░██░░░████░░░ *
+ * ░░██████░░░████████░░░████░░░ *
+ * ░░██░░██░░░████░░██░░░████░░░ *
+ * ░░██░░██░░░████░░██░░░████░░░ *
+ * ░░░░░░█████████░░█████████░░░ *
+ * ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ *
+ * ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ *
+ *********************************/
+
+// LICENSE
+// NounsAuctionHouse.sol is a modified version of Zora's AuctionHouse.sol:
+// https://github.com/ourzora/auction-house/blob/54a12ec1a6cf562e49f0a4917990474b11350a2d/contracts/AuctionHouse.sol
+//
+// AuctionHouse.sol source code Copyright Zora licensed under the GPL-3.0 license.
+// With modifications by Nounders DAO.
+
+pragma solidity ^0.8.6;
+
+import { PausableUpgradeable } from '@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol';
+import { ReentrancyGuardUpgradeable } from '@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol';
+import { OwnableUpgradeable } from '@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol';
+import { IERC20 } from '@openzeppelin/contracts/token/ERC20/IERC20.sol';
+import { INounsAuctionHouse } from './interfaces/INounsAuctionHouse.sol';
+import { INounsToken } from './interfaces/INounsToken.sol';
+import { IWETH } from './interfaces/IWETH.sol';
+
+contract NounsAuctionHouse is INounsAuctionHouse, PausableUpgradeable, ReentrancyGuardUpgradeable, OwnableUpgradeable {
+    // The Nouns ERC721 token contract
+    INounsToken public nouns;
+
+    // The address of the WETH contract
+    address public weth;
+
+    // The minimum amount of time left in an auction after a new bid is created
+    uint256 public timeBuffer;
+
+    // The minimum price accepted in an auction
+    uint256 public reservePrice;
+
+    // The minimum percentage difference between the last bid amount and the current bid
+    uint8 public minBidIncrementPercentage;
+
+    // The duration of a single auction
+    uint256 public duration;
+
+    // The active auction
+    INounsAuctionHouse.Auction public auction;
+
+    /**
+     * @notice Initialize the auction house and base contracts,
+     * populate configuration values, and pause the contract.
+     * @dev This function can only be called once.
+     */
+    function initialize(
+        INounsToken _nouns,
+        address _weth,
+        uint256 _timeBuffer,
+        uint256 _reservePrice,
+        uint8 _minBidIncrementPercentage,
+        uint256 _duration
+    ) external initializer {
+        __Pausable_init();
+        __ReentrancyGuard_init();
+        __Ownable_init();
+
+        _pause();
+
+        nouns = _nouns;
+        weth = _weth;
+        timeBuffer = _timeBuffer;
+        reservePrice = _reservePrice;
+        minBidIncrementPercentage = _minBidIncrementPercentage;
+        duration = _duration;
+    }
+
+    /**
+     * @notice Settle the current auction, mint a new Noun, and put it up for auction.
+     */
+    function settleCurrentAndCreateNewAuction() external override nonReentrant whenNotPaused {
+        _settleAuction();
+        _createAuction();
+    }
+
+    /**
+     * @notice Settle the current auction.
+     * @dev This function can only be called when the contract is paused.
+     */
+    function settleAuction() external override whenPaused nonReentrant {
+        _settleAuction();
+    }
+
+    /**
+     * @notice Create a bid for a Noun, with a given amount.
+     * @dev This contract only accepts payment in ETH.
+     */
+    function createBid(uint256 nounId) external payable override nonReentrant {
+        INounsAuctionHouse.Auction memory _auction = auction;
+
+        require(_auction.nounId == nounId, 'Noun not up for auction');
+        require(block.timestamp < _auction.endTime, 'Auction expired');
+        require(msg.value >= reservePrice, 'Must send at least reservePrice');
+        require(
+            msg.value >= _auction.amount + ((_auction.amount * minBidIncrementPercentage) / 100),
+            'Must send more than last bid by minBidIncrementPercentage amount'
+        );
+
+        address payable lastBidder = _auction.bidder;
+
+        // Refund the last bidder, if applicable
+        if (lastBidder != address(0)) {
+            _safeTransferETHWithFallback(lastBidder, _auction.amount);
+        }
+
+        auction.amount = msg.value;
+        auction.bidder = payable(msg.sender);
+
+        // Extend the auction if the bid was received within `timeBuffer` of the auction end time
+        bool extended = _auction.endTime - block.timestamp < timeBuffer;
+        if (extended) {
+            auction.endTime = _auction.endTime = block.timestamp + timeBuffer;
+        }
+
+        emit AuctionBid(_auction.nounId, msg.sender, msg.value, extended);
+
+        if (extended) {
+            emit AuctionExtended(_auction.nounId, _auction.endTime);
+        }
+    }
+
+    /**
+     * @notice Pause the Nouns auction house.
+     * @dev This function can only be called by the owner when the
+     * contract is unpaused. While no new auctions can be started when paused,
+     * anyone can settle an ongoing auction.
+     */
+    function pause() external override onlyOwner {
+        _pause();
+    }
+
+    /**
+     * @notice Unpause the Nouns auction house.
+     * @dev This function can only be called by the owner when the
+     * contract is paused. If required, this function will start a new auction.
+     */
+    function unpause() external override onlyOwner {
+        _unpause();
+
+        if (auction.startTime == 0 || auction.settled) {
+            _createAuction();
+        }
+    }
+
+    /**
+     * @notice Set the auction time buffer.
+     * @dev Only callable by the owner.
+     */
+    function setTimeBuffer(uint256 _timeBuffer) external override onlyOwner {
+        timeBuffer = _timeBuffer;
+
+        emit AuctionTimeBufferUpdated(_timeBuffer);
+    }
+
+    /**
+     * @notice Set the auction reserve price.
+     * @dev Only callable by the owner.
+     */
+    function setReservePrice(uint256 _reservePrice) external override onlyOwner {
+        reservePrice = _reservePrice;
+
+        emit AuctionReservePriceUpdated(_reservePrice);
+    }
+
+    /**
+     * @notice Set the auction minimum bid increment percentage.
+     * @dev Only callable by the owner.
+     */
+    function setMinBidIncrementPercentage(uint8 _minBidIncrementPercentage) external override onlyOwner {
+        minBidIncrementPercentage = _minBidIncrementPercentage;
+
+        emit AuctionMinBidIncrementPercentageUpdated(_minBidIncrementPercentage);
+    }
+
+    /**
+     * @notice Create an auction.
+     * @dev Store the auction details in the `auction` state variable and emit an AuctionCreated event.
+     * If the mint reverts, the minter was updated without pausing this contract first. To remedy this,
+     * catch the revert and pause this contract.
+     */
+    function _createAuction() internal {
+        try nouns.mint() returns (uint256 nounId) {
+            uint256 startTime = block.timestamp;
+            uint256 endTime = startTime + duration;
+
+            auction = Auction({
+                nounId: nounId,
+                amount: 0,
+                startTime: startTime,
+                endTime: endTime,
+                bidder: payable(0),
+                settled: false
+            });
+
+            emit AuctionCreated(nounId, startTime, endTime);
+        } catch Error(string memory) {
+            _pause();
+        }
+    }
+
+    /**
+     * @notice Settle an auction, finalizing the bid and paying out to the owner.
+     * @dev If there are no bids, the Noun is burned.
+     */
+    function _settleAuction() internal {
+        INounsAuctionHouse.Auction memory _auction = auction;
+
+        require(_auction.startTime != 0, "Auction hasn't begun");
+        require(!_auction.settled, 'Auction has already been settled');
+        require(block.timestamp >= _auction.endTime, "Auction hasn't completed");
+
+        auction.settled = true;
+
+        if (_auction.bidder == address(0)) {
+            nouns.burn(_auction.nounId);
+        } else {
+            nouns.transferFrom(address(this), _auction.bidder, _auction.nounId);
+        }
+
+        if (_auction.amount > 0) {
+            _safeTransferETHWithFallback(owner(), _auction.amount);
+        }
+
+        emit AuctionSettled(_auction.nounId, _auction.bidder, _auction.amount);
+    }
+
+    /**
+     * @notice Transfer ETH. If the ETH transfer fails, wrap the ETH and try send it as WETH.
+     */
+    function _safeTransferETHWithFallback(address to, uint256 amount) internal {
+        if (!_safeTransferETH(to, amount)) {
+            IWETH(weth).deposit{ value: amount }();
+            IERC20(weth).transfer(to, amount);
+        }
+    }
+
+    /**
+     * @notice Transfer ETH and return the success status.
+     * @dev This function only forwards 30,000 gas to the callee.
+     */
+    function _safeTransferETH(address to, uint256 value) internal returns (bool) {
+        (bool success, ) = to.call{ value: value, gas: 30_000 }(new bytes(0));
+        return success;
+    }
+}
+// SPDX-License-Identifier: GPL-3.0
+
+/// @title Interface for Noun Auction Houses
+
+/*********************************
+ * ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ *
+ * ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ *
+ * ░░░░░░█████████░░█████████░░░ *
+ * ░░░░░░██░░░████░░██░░░████░░░ *
+ * ░░██████░░░████████░░░████░░░ *
+ * ░░██░░██░░░████░░██░░░████░░░ *
+ * ░░██░░██░░░████░░██░░░████░░░ *
+ * ░░░░░░█████████░░█████████░░░ *
+ * ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ *
+ * ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ *
+ *********************************/
+
+pragma solidity ^0.8.4;
+
+interface INounsAuctionHouse {
+    struct Auction {
+        // ID for the Noun (ERC721 token ID)
+        uint256 nounId;
+        // The current highest bid amount
+        uint256 amount;
+        // The time that the auction started
+        uint256 startTime;
+        // The time that the auction is scheduled to end
+        uint256 endTime;
+        // The address of the current highest bid
+        address payable bidder;
+        // Whether or not the auction has been settled
+        bool settled;
+    }
+
+    event AuctionCreated(uint256 indexed nounId, uint256 startTime, uint256 endTime);
+
+    event AuctionBid(uint256 indexed nounId, address sender, uint256 value, bool extended);
+
+    event AuctionExtended(uint256 indexed nounId, uint256 endTime);
+
+    event AuctionSettled(uint256 indexed nounId, address winner, uint256 amount);
+
+    event AuctionTimeBufferUpdated(uint256 timeBuffer);
+
+    event AuctionReservePriceUpdated(uint256 reservePrice);
+
+    event AuctionMinBidIncrementPercentageUpdated(uint256 minBidIncrementPercentage);
+
+    function settleAuction() external;
+
+    function settleCurrentAndCreateNewAuction() external;
+
+    function createBid(uint256 nounId) external payable;
+
+    function pause() external;
+
+    function unpause() external;
+
+    function setTimeBuffer(uint256 timeBuffer) external;
+
+    function setReservePrice(uint256 reservePrice) external;
+
+    function setMinBidIncrementPercentage(uint8 minBidIncrementPercentage) external;
+}
+// SPDX-License-Identifier: GPL-3.0
+
+/// @title Interface for NounsToken
+
+/*********************************
+ * ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ *
+ * ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ *
+ * ░░░░░░█████████░░█████████░░░ *
+ * ░░░░░░██░░░████░░██░░░████░░░ *
+ * ░░██████░░░████████░░░████░░░ *
+ * ░░██░░██░░░████░░██░░░████░░░ *
+ * ░░██░░██░░░████░░██░░░████░░░ *
+ * ░░░░░░█████████░░█████████░░░ *
+ * ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ *
+ * ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ *
+ *********************************/
+
+pragma solidity ^0.8.4;
+
+import { IERC721 } from '@openzeppelin/contracts/token/ERC721/IERC721.sol';
+import { INounsDescriptorMinimal } from './INounsDescriptorMinimal.sol';
+import { INounsSeeder } from './INounsSeeder.sol';
+
+interface INounsToken is IERC721 {
+    event NounCreated(uint256 indexed tokenId, INounsSeeder.Seed seed);
+
+    event NounBurned(uint256 indexed tokenId);
+
+    event NoundersDAOUpdated(address noundersDAO);
+
+    event MinterUpdated(address minter);
+
+    event MinterLocked();
+
+    event DescriptorUpdated(INounsDescriptorMinimal descriptor);
+
+    event DescriptorLocked();
+
+    event SeederUpdated(INounsSeeder seeder);
+
+    event SeederLocked();
+
+    function mint() external returns (uint256);
+
+    function burn(uint256 tokenId) external;
+
+    function dataURI(uint256 tokenId) external returns (string memory);
+
+    function setNoundersDAO(address noundersDAO) external;
+
+    function setMinter(address minter) external;
+
+    function lockMinter() external;
+
+    function setDescriptor(INounsDescriptorMinimal descriptor) external;
+
+    function lockDescriptor() external;
+
+    function setSeeder(INounsSeeder seeder) external;
+
+    function lockSeeder() external;
+}
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.4;
+
+interface IWETH {
+    function deposit() external payable;
+
+    function withdraw(uint256 wad) external;
+
+    function transfer(address to, uint256 value) external returns (bool);
+}
